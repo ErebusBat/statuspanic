3918cb7619c2d9f8357f73cc598d60421dc93d85
Jacob biosshadow Kuehndorf <biosshadow@gmail.com>
2010-07-04 17:39:11 -0500

    added my name and site changed to my zip code

--- modules/meta.module.php
+++ modules/meta.module.php
@@ -1,4 +1,5 @@
 <div class='jumbo'>
     Created by <a href='http://allynbauer.com'>Allyn Bauer</a><br />
-    Get it at <a href='http://github.com/ajb/statuspanic'>GitHub</a>
+	Modified by <a href='https://biosshadow.com'>Jacob "biosshadow"</a><br />
+    Get it at <a href='http://github.com/biosshadow/statuspanic'>GitHub</a>
 </div>
